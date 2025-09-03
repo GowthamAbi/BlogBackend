d27ae2025f209a7f8bb66b7aa52946ed464eac85
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-09-03 20:37:40 +0530

    login

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,7 +1,8 @@
 import  jwt  from "jsonwebtoken";
 
 const auth=(req,res,next)=>{
-    const token=req.headers.authorization;
+    const token=req.header("x-auth-token")
+     if (!token) return res.status(401).json({ message: "No token, authorization denied" });
     try {
         
         jwt.verify(token,process.env.SECRET_KEY)
