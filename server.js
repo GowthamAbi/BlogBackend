39efdc51667b274e1db150c0119d603c66856954
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-11-10 02:13:14 +0530

    finish

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ await connectedDB()
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:3000"
+    "https://gowthamblogs.netlify.app/"||"http://localhost:5173"
   ],
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
